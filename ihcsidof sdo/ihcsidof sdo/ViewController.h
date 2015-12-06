c2cb237091fa193d0b7c958451c90718ccfd475f
刘晓龙 <9871208@sin.com>
2015-12-06 13:26:14 +0800

    认为二位人味儿

---
+++ ihcsidof sdo/ihcsidof sdo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ihcsidof sdo
+//
+//  Created by 刘晓龙 on 15/12/6.
+//  Copyright (c) 2015年 hlzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ihcsidof sdo/ihcsidof sdo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ihcsidof sdo
+//
+//  Created by 刘晓龙 on 15/12/6.
+//  Copyright (c) 2015年 hlzx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
